3847c08027677b7d0b733f98b27ad44092f5518c
Awesome Rustacean <geniusinrust@gmail.com>
2025-12-21 22:40:58 +0300

    Implement my own SpinLock lock. It allows a thread to keep on trying to lock a value held by another thread until the other thread releases the lock, So the thread doesn't sleep when another thread isholding the lock. I use the spin loop hint to tell the processor that I'm spinning while waiting for something to change

---
+++ src/locks/mod.rs
@@ -0,0 +1,67 @@
+#![allow(unused)]
+
+use std::{
+    cell::UnsafeCell,
+    ops::{Deref, DerefMut},
+    sync::atomic::{
+        AtomicBool,
+        Ordering::{Acquire, Relaxed, Release},
+    },
+};
+
+#[derive(Debug)]
+pub struct Guard<'a, T> {
+    lock: &'a SpinLock<T>,
+}
+
+impl<T> Deref for Guard<'_, T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        // Safety: The very existence of this Guard, guarantees we've exclusively locked the lock.
+        unsafe { &*self.lock.value.get() }
+    }
+}
+
+impl<T> DerefMut for Guard<'_, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        // Safety: The very existence of this Guard, guarantees we've exclusively locked the lock.
+        unsafe { &mut *self.lock.value.get() }
+    }
+}
+
+impl<T> Drop for Guard<'_, T> {
+    fn drop(&mut self) {
+        self.lock.locked.store(false, Release);
+    }
+}
+
+#[derive(Debug)]
+pub struct SpinLock<T> {
+    locked: AtomicBool,
+    value: UnsafeCell<T>,
+}
+
+unsafe impl<T> Sync for SpinLock<T> where T: Send {}
+
+impl<T> SpinLock<T> {
+    pub const fn new(value: T) -> Self {
+        Self {
+            locked: AtomicBool::new(false),
+            value: UnsafeCell::new(value),
+        }
+    }
+
+    pub fn lock<'a>(&'a self) -> Guard<'a, T> {
+        while self.locked.swap(true, Acquire) {
+            std::hint::spin_loop();
+        }
+
+        Guard { lock: self }
+    }
+
+    /// Safety: The &mut T from lock() must be gone!
+    /// (And no cheating by keeping reference to fields of that T around!)
+    pub unsafe fn unlock(&self) {
+        self.locked.store(false, Release);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,26 @@
-fn main() {}
+use std::thread;
+
+use crate::locks::SpinLock;
+
+mod channels;
+mod locks;
+mod pointers;
+
+fn main() {
+    let spin: SpinLock<Vec<i32>> = SpinLock::new(Vec::new());
+
+    thread::scope(|s| {
+        s.spawn(|| {
+            spin.lock().push(3);
+        });
+        s.spawn(|| {
+            let mut value = spin.lock();
+            value.push(2);
+            value.push(1);
+            value.push(4);
+        });
+    });
+    let g = spin.lock();
+    
+    assert!(g.as_slice() == [3, 2, 1, 4] || g.as_slice() == [2, 1, 4, 3]);
+}
