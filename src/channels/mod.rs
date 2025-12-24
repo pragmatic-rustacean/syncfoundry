47d22230bb43d45fd65b5cb710010abea4dd383a
Awesome Rustacean <geniusinrust@gmail.com>
2025-12-24 11:53:10 +0300

    Safer and a better implementation of the channel

--- src/channels/mod.rs
+++ src/channels/mod.rs
@@ -1,35 +1,64 @@
 #![allow(unused)]
 
 use std::{
+    cell::UnsafeCell,
     collections::VecDeque,
-    sync::{Condvar, Mutex},
+    mem::MaybeUninit,
+    sync::{
+        Condvar, Mutex,
+        atomic::{
+            AtomicBool,
+            Ordering::{Acquire, Relaxed, Release},
+        },
+    },
 };
 
-struct Channel<T> {
-    queue: Mutex<VecDeque<T>>,
-    ready: Condvar,
+pub struct Channel<T> {
+    message: UnsafeCell<MaybeUninit<T>>,
+    is_ready: AtomicBool,
+    in_use: AtomicBool,
+}
+
+unsafe impl<T> Sync for Channel<T> where T: Send {}
+impl<T> Drop for Channel<T> {
+    fn drop(&mut self) {
+        if *self.is_ready.get_mut() {
+            unsafe {
+                self.message.get_mut().assume_init_drop();
+            }
+        }
+    }
 }
 
 impl<T> Channel<T> {
-    pub fn new() -> Self {
+    pub const fn new() -> Self {
         Self {
-            queue: Mutex::new(VecDeque::new()),
-            ready: Condvar::new(),
+            message: UnsafeCell::new(MaybeUninit::uninit()),
+            is_ready: AtomicBool::new(false),
+            in_use: AtomicBool::new(false),
         }
     }
 
+    /// Safety: Only call this once!
     pub fn send(&self, message: T) {
-        self.queue.lock().unwrap().push_back(message);
-        self.ready.notify_one();
+        if self.in_use.swap(true, Relaxed) {
+            panic!("Can't send more than one message.")
+        }
+        unsafe {
+            (*self.message.get()).write(message);
+        };
+        self.is_ready.store(true, Release);
     }
 
+    pub fn is_ready(&self) -> bool {
+        self.is_ready.load(Relaxed)
+    }
+
+    /// Safety: Only call this once, and only after is_ready returns true.
     pub fn receive(&self) -> T {
-        let mut data = self.queue.lock().unwrap();
-        loop {
-            if let Some(message) = data.pop_front() {
-                return message;
-            }
-            data = self.ready.wait(data).unwrap()
+        if !self.is_ready.swap(false, Acquire) {
+            panic!("No message yet!!")
         }
+        unsafe { (*self.message.get()).assume_init_read() }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,25 @@
 use std::thread;
 
-use crate::locks::SpinLock;
+use crate::channels::Channel;
+// use crate::locks::SpinLock;
 
 mod channels;
 mod locks;
 mod pointers;
 
 fn main() {
-    let spin: SpinLock<Vec<i32>> = SpinLock::new(Vec::new());
+    let channel = Channel::new();
+    let t = thread::current();
 
     thread::scope(|s| {
         s.spawn(|| {
-            spin.lock().push(3);
+            channel.send("Hello, my dear James");
+            t.unpark();
         });
-        s.spawn(|| {
-            let mut value = spin.lock();
-            value.push(2);
-            value.push(1);
-            value.push(4);
-        });
+
+        while !channel.is_ready() {
+            thread::park();
+        }
     });
-    let g = spin.lock();
-    
-    assert!(g.as_slice() == [3, 2, 1, 4] || g.as_slice() == [2, 1, 4, 3]);
+    println!("Results: {}", channel.receive())
 }
